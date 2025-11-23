3ec0a40416432a1da20a8c0a92bb0c7e66f2c885
benjaflores1343 <benja.floresg@duocuc.cl>
2025-11-23 15:20:23 -0300

    CREAMOS TEST

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -5,17 +5,12 @@ interface Props {
   robots: robotsProps[];
 }
 
-export const ImageList: FC<Props> = ({ robots }) => {
-  return (
-    <div className="image-list">
-      {robots.map((robot) => (
-        <div key={robot.id} className="robot-card">
-          <img src={robot.avatar} alt={robot.name} />
-          <h3>{robot.name}</h3>
-          <p>ID: {robot.id}</p>
-          <p>Weapon: {robot.weapon}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+import { UserService } from './src/services/UserService';
+UserService.initializeDefaultUsers();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+  
